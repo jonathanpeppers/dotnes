c53f081fbbcaedf643113bedd06de133e5011302
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-11-22 21:16:07 -0600

    File -> New sample and stubbed NESLib
    Links:
    * https://8bitworkshop.com/v3.8.0/?platform=nes&file=hello.c
    * https://github.com/clbr/neslib/blob/master/neslib.h

---
+++ dotnes.sample/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Based on: https://8bitworkshop.com/v3.8.0/?platform=nes&file=hello.c
+
+A simple "hello world" example.
+Set the screen background color and palette colors.
+Then write a message to the nametable.
+Finally, turn on the PPU to display video.
+*/
+
+// set palette colors
+pal_col(0, 0x02);   // set screen to dark blue
+pal_col(1, 0x14);   // fuchsia
+pal_col(2, 0x20);   // grey
+pal_col(3, 0x30);   // white
+
+
+// write text to name table
+vram_adr(NTADR_A(2, 2));            // set address
+vram_write("HELLO, WORLD!", 13);    // write bytes to video RAM
+
+// enable PPU rendering (turn on screen)
+ppu_on_all();
+
+// infinite loop
+while (true) ;
---
+++ neslib/NESLib.cs
@@ -0,0 +1,59 @@
+﻿namespace NES;
+
+/// <summary>
+/// Recommended use via `using static NES.NESLib;`
+/// Based on: https://github.com/clbr/neslib/blob/master/neslib.h
+/// </summary>
+public static class NESLib
+{
+    /// <summary>
+    /// set a palette entry, index is 0..31
+    /// </summary>
+    public static void pal_col(byte index, byte color) { }
+
+    /// <summary>
+    /// set vram pointer to write operations if you need to write some data to vram
+    /// </summary>
+    public static void vram_adr(ushort adr) { }
+
+    /// <summary>
+    /// write a block to current address of vram, works only when rendering is turned off
+    /// </summary>
+    public static void vram_write(string src, ushort size) { }
+
+    /// <summary>
+    /// turn on bg, spr
+    /// </summary>
+    public static void ppu_on_all() { }
+
+    public const ushort NAMETABLE_A = 0x2000;
+    public const ushort NAMETABLE_B = 0x2400;
+    public const ushort NAMETABLE_C = 0x2800;
+    public const ushort NAMETABLE_D = 0x2c00;
+
+    // TODO: Macros below should be computed at compile-time and methods removed
+
+    /// <summary>
+    /// macro to calculate nametable address from X,Y in compile time
+    /// #define NTADR_A(x,y)	 	(NAMETABLE_A|(((y)<<5)|(x)))
+    /// </summary>
+    public static ushort NTADR_A(byte x, byte y) => (ushort)(NAMETABLE_A | (((y) << 5) | (x)));
+
+    /// <summary>
+    /// macro to calculate nametable address from X,Y in compile time
+    /// #define NTADR_B(x,y) 		(NAMETABLE_B|(((y)<<5)|(x)))
+    /// </summary>
+    public static ushort NTADR_B(byte x, byte y) => (ushort)(NAMETABLE_B | (((y) << 5) | (x)));
+
+    /// <summary>
+    /// macro to calculate nametable address from X,Y in compile time
+    /// #define NTADR_C(x,y) 		(NAMETABLE_C|(((y)<<5)|(x)))
+    /// </summary>
+    public static ushort NTADR_C(byte x, byte y) => (ushort)(NAMETABLE_C | (((y) << 5) | (x)));
+
+    /// <summary>
+    /// macro to calculate nametable address from X,Y in compile time
+    /// #define NTADR_D(x,y) 		(NAMETABLE_D|(((y)<<5)|(x)))
+    /// </summary>
+    public static ushort NTADR_D(byte x, byte y) => (ushort)(NAMETABLE_D | (((y) << 5) | (x)));
+}
