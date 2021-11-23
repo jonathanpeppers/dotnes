52e7bb3b019b23680a422d0dd15d96908884686f
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-11-22 22:31:41 -0600

    Update CompileToNES.cs

--- dotnes/Tasks/CompileToNES.cs
+++ dotnes/Tasks/CompileToNES.cs
@@ -2,8 +2,18 @@
 
 public class CompileToNES : Task
 {
+    [Required]
+    public string TargetPath { get; set; } = "";
+
+    [Required]
+    public string OutputPath { get; set; } = "";
+
     public override bool Execute()
     {
+        using var fs = File.Create(OutputPath);
+        using var writer = new NESWriter(fs);
+        writer.Write();
+
         return !Log.HasLoggedErrors;
     }
 }
