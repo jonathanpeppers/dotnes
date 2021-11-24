d8b74f8a4750851ff9690216513d92dbaaba3c3b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-11-23 22:36:18 -0600

    Use SRM to iterate over IL instructions
    Can locate a `static void Main()`

---
+++ dotnes.tests/IL2AssemblyTests.cs
@@ -0,0 +1,39 @@
+﻿namespace dotnes.tests;
+
+public class IL2AssemblyTests
+{
+    readonly string path;
+
+    public IL2AssemblyTests()
+    {
+        var dir = Path.GetDirectoryName(GetType().Assembly.Location)!;
+        path = Path.Combine(dir, "dotnes.sample.dll");
+    }
+
+    [Fact]
+    public void StaticVoidMain()
+    {
+        using var il = new IL2Assembly(path);
+        Assert.Equal(@"Ldc_i4_0
+Ldc_i4_2
+Call pal_col
+Call pal_col
+Call pal_col
+Call pal_col
+Stloc_3
+Nop
+Nop
+Stloc_0
+Call vram_adr
+Nop
+Cpobj
+Nop
+Nop
+Stloc_0
+Nop
+Call ppu_on_all
+Ldc_i4_1
+Stloc_0
+Br_s", il.ReadStaticVoidMain());
+    }
+}
---
+++ dotnes/Utilities/IL2Assembly.cs
@@ -0,0 +1,170 @@
+﻿using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace dotnes;
+
+class IL2Assembly : IDisposable
+{
+    readonly PEReader pe;
+    readonly MetadataReader reader;
+
+    public IL2Assembly(string path)
+    {
+        pe = new PEReader(File.OpenRead(path));
+        reader = pe.GetMetadataReader();
+    }
+
+    /// <summary>
+    /// Based on: https://github.com/icsharpcode/ILSpy/blob/8c508d9bbbc6a21cc244e930122ff5bca19cd11c/ILSpy/Analyzers/Builtin/MethodUsesAnalyzer.cs#L51
+    /// </summary>
+    public string ReadStaticVoidMain()
+    {
+        var builder = new StringBuilder();
+        foreach (var h in reader.MethodDefinitions)
+        {
+            var method = reader.GetMethodDefinition(h);
+            if ((method.Attributes & MethodAttributes.Static) == 0)
+                continue;
+
+            var methodName = reader.GetString(method.Name);
+            if (methodName == "Main" || methodName == "<Main>$")
+            {
+                var body = pe.GetMethodBody(method.RelativeVirtualAddress);
+                var blob = body.GetILReader();
+
+                while (blob.RemainingBytes > 0)
+                {
+                    if (builder.Length > 0)
+                        builder.AppendLine();
+
+                    ILOpCode opCode = DecodeOpCode(ref blob);
+                    builder.Append(opCode);
+
+                    OperandType operandType = GetOperandType(opCode);
+
+                    switch (operandType)
+                    {
+                        case OperandType.Field:
+                        case OperandType.Method:
+                        case OperandType.Sig:
+                        case OperandType.Tok:
+                            var member = MetadataTokens.EntityHandle(blob.ReadInt32());
+                            if (member.IsNil)
+                                continue;
+
+                            switch (member.Kind)
+                            {
+                                case HandleKind.TypeDefinition:
+                                    builder.Append(' ');
+                                    builder.Append(reader.GetString(reader.GetTypeDefinition((TypeDefinitionHandle)member).Name));
+                                    break;
+                                case HandleKind.TypeReference:
+                                    builder.Append(' ');
+                                    builder.Append(reader.GetString(reader.GetTypeReference((TypeReferenceHandle)member).Name));
+                                    break;
+                                case HandleKind.MethodDefinition:
+                                    builder.Append(' ');
+                                    builder.Append(reader.GetString(reader.GetMethodDefinition((MethodDefinitionHandle)member).Name));
+                                    break;
+                                case HandleKind.MemberReference:
+                                    builder.Append(' ');
+                                    builder.Append(reader.GetString(reader.GetMemberReference((MemberReferenceHandle)member).Name));
+                                    break;
+                                case HandleKind.FieldDefinition:
+                                    builder.Append(' ');
+                                    builder.Append(reader.GetString(reader.GetFieldDefinition((FieldDefinitionHandle)member).Name));
+                                    break;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+
+                    SkipOperand(ref blob, operandType);
+                }
+            }
+        }
+        return builder.ToString();
+    }
+
+    static ILOpCode DecodeOpCode(ref BlobReader blob)
+    {
+        byte opCodeByte = blob.ReadByte();
+        return (ILOpCode)(opCodeByte == 0xFE ? 0xFE00 + blob.ReadByte() : opCodeByte);
+    }
+
+    static readonly byte[] operandTypes = { (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.ShortVariable, (byte)OperandType.ShortVariable, (byte)OperandType.ShortVariable, (byte)OperandType.ShortVariable, (byte)OperandType.ShortVariable, (byte)OperandType.ShortVariable, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.ShortI, (byte)OperandType.I, (byte)OperandType.I8, (byte)OperandType.ShortR, (byte)OperandType.R, 255, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.Method, (byte)OperandType.Method, (byte)OperandType.Sig, (byte)OperandType.None, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.BrTarget, (byte)OperandType.Switch, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.Method, (byte)OperandType.Type, (byte)OperandType.Type, (byte)OperandType.String, (byte)OperandType.Method, (byte)OperandType.Type, (byte)OperandType.Type, (byte)OperandType.None, 255, 255, (byte)OperandType.Type, (byte)OperandType.None, (byte)OperandType.Field, (byte)OperandType.Field, (byte)OperandType.Field, (byte)OperandType.Field, (byte)OperandType.Field, (byte)OperandType.Field, (byte)OperandType.Type, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.Type, (byte)OperandType.Type, (byte)OperandType.None, (byte)OperandType.Type, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.Type, (byte)OperandType.Type, (byte)OperandType.Type, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, 255, 255, 255, 255, 255, 255, 255, (byte)OperandType.Type, (byte)OperandType.None, 255, 255, (byte)OperandType.Type, 255, 255, 255, 255, 255, 255, 255, 255, 255, (byte)OperandType.Tok, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.BrTarget, (byte)OperandType.ShortBrTarget, (byte)OperandType.None, (byte)OperandType.None, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.Method, (byte)OperandType.Method, 255, (byte)OperandType.Variable, (byte)OperandType.Variable, (byte)OperandType.Variable, (byte)OperandType.Variable, (byte)OperandType.Variable, (byte)OperandType.Variable, (byte)OperandType.None, 255, (byte)OperandType.None, (byte)OperandType.ShortI, (byte)OperandType.None, (byte)OperandType.None, (byte)OperandType.Type, (byte)OperandType.Type, (byte)OperandType.None, (byte)OperandType.None, 255, (byte)OperandType.None, 255, (byte)OperandType.Type, (byte)OperandType.None, (byte)OperandType.None, };
+
+    static OperandType GetOperandType(ILOpCode opCode)
+    {
+        ushort index = (ushort)((((int)opCode & 0x200) >> 1) | ((int)opCode & 0xff));
+        if (index >= operandTypes.Length)
+            return (OperandType)255;
+        return (OperandType)operandTypes[index];
+    }
+
+    static void SkipOperand(ref BlobReader blob, OperandType operandType)
+    {
+        switch (operandType)
+        {
+            // 64-bit
+            case OperandType.I8:
+            case OperandType.R:
+                blob.Offset += 8;
+                break;
+            // 32-bit
+            case OperandType.BrTarget:
+            case OperandType.Field:
+            case OperandType.Method:
+            case OperandType.I:
+            case OperandType.Sig:
+            case OperandType.String:
+            case OperandType.Tok:
+            case OperandType.Type:
+            case OperandType.ShortR:
+                blob.Offset += 4;
+                break;
+            // (n + 1) * 32-bit
+            case OperandType.Switch:
+                uint n = blob.ReadUInt32();
+                blob.Offset += (int)(n * 4);
+                break;
+            // 16-bit
+            case OperandType.Variable:
+                blob.Offset += 2;
+                break;
+            // 8-bit
+            case OperandType.ShortVariable:
+            case OperandType.ShortBrTarget:
+            case OperandType.ShortI:
+                blob.Offset++;
+                break;
+        }
+    }
+
+    enum OperandType
+    {
+        BrTarget,
+        Field,
+        I,
+        I8,
+        Method,
+        None,
+        R = 7,
+        Sig = 9,
+        String,
+        Switch,
+        Tok,
+        Type,
+        Variable,
+        ShortBrTarget,
+        ShortI,
+        ShortR,
+        ShortVariable
+    }
+
+    public void Dispose() => pe.Dispose();
+}
