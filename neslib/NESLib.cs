e83f2dace31a4a366141e8aa1f9bda32ada62fff
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-12-04 11:37:11 -0600

    Added missing pal or ppu neslib stubs

--- neslib/NESLib.cs
+++ neslib/NESLib.cs
@@ -27,6 +27,89 @@ public static class NESLib
     public static void pal_col(byte index, byte color) { }
 
     /// <summary>
+    /// reset palette to $0f
+    /// </summary>
+    public static void pal_clear() { }
+
+    /// <summary>
+    /// set virtual bright both for sprites and background, 0 is black, 4 is normal, 8 is white
+    /// </summary>
+    public static void pal_bright(byte bright) { }
+
+    /// <summary>
+    /// set virtual bright for sprites only
+    /// </summary>
+    public static void pal_spr_bright(byte bright) { }
+
+    /// <summary>
+    /// set virtual bright for sprites background only
+    /// </summary>
+    public static void pal_bg_bright(byte bright) { }
+
+
+
+
+
+    /// <summary>
+    /// wait actual TV frame, 50hz for PAL, 60hz for NTSC
+    /// </summary>
+    public static void ppu_wait_nmi() { }
+
+    /// <summary>
+    /// wait virtual frame, it is always 50hz, frame-to-frame in PAL, frameskip in NTSC
+    /// </summary>
+    public static void ppu_wait_frame() { }
+
+    /// <summary>
+    /// turn off rendering, nmi still enabled when rendering is disabled
+    /// </summary>
+    public static void ppu_off() { }
+
+    /// <summary>
+    /// turn on bg, spr
+    /// </summary>
+    public static void ppu_on_all() { }
+
+    /// <summary>
+    /// turn on bg only
+    /// </summary>
+    public static void ppu_on_bg() { }
+
+    /// <summary>
+    /// turn on spr only
+    /// </summary>
+    public static void ppu_on_spr() { }
+
+    /// <summary>
+    /// set PPU_MASK directly
+    /// </summary>
+    public static void ppu_mask(byte mask) { }
+
+    /// <summary>
+    /// get current video system, 0 for PAL, not 0 for NTSC
+    /// </summary>
+    public static byte ppu_system() => default;
+
+    /// <summary>
+    /// Return an 8-bit counter incremented at each vblank
+    /// </summary>
+    public static byte nesclock() => default;
+
+    /// <summary>
+    /// get the internal ppu ctrl cache var for manual writing
+    /// </summary>
+    public static byte get_ppu_ctrl_var() => default;
+
+    /// <summary>
+    /// set the internal ppu ctrl cache var for manual writing
+    /// </summary>
+    public static void set_ppu_ctrl_var(byte var) { }
+
+
+
+
+
+    /// <summary>
     /// set vram pointer to write operations if you need to write some data to vram
     /// </summary>
     public static void vram_adr(ushort adr) { }
@@ -35,11 +118,6 @@ public static class NESLib
     /// write a block to current address of vram, works only when rendering is turned off
     /// </summary>
     public static void vram_write(string src, ushort size) { }
-
-    /// <summary>
-    /// turn on bg, spr
-    /// </summary>
-    public static void ppu_on_all() { }
 
     public const ushort NAMETABLE_A = 0x2000;
     public const ushort NAMETABLE_B = 0x2400;
