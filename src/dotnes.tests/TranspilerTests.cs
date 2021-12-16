58764fbf6d7a1543b2b3bc139db4eccc3359e892
Jonathan Peppers <jonathan.peppers@microsoft.com>
2021-12-15 22:48:12 -0600

    Update TranspilerTests.cs

--- src/dotnes.tests/TranspilerTests.cs
+++ src/dotnes.tests/TranspilerTests.cs
@@ -29,12 +29,24 @@ ILInstruction { OpCode = Call, Integer = , String = NES.NESLib::vram_write }
 ILInstruction { OpCode = Call, Integer = , String = NES.NESLib::ppu_on_all }
 ILInstruction { OpCode = Br_s, Integer = 254, String =  }"
         },
+        { "hello.sub",
+@"ILInstruction { OpCode = Call, Integer = , String = Program::<<Main>$>g__setup_pallette|0_0 }
+ILInstruction { OpCode = Ldc_i4_2, Integer = , String =  }
+ILInstruction { OpCode = Ldc_i4_2, Integer = , String =  }
+ILInstruction { OpCode = Call, Integer = , String = NES.NESLib::NTADR_A }
+ILInstruction { OpCode = Call, Integer = , String = NES.NESLib::vram_adr }
+ILInstruction { OpCode = Ldstr, Integer = , String = HELLO, .NET! }
+ILInstruction { OpCode = Ldc_i4_s, Integer = 13, String =  }
+ILInstruction { OpCode = Call, Integer = , String = NES.NESLib::vram_write }
+ILInstruction { OpCode = Call, Integer = , String = NES.NESLib::ppu_on_all }
+ILInstruction { OpCode = Br_s, Integer = 254, String =  }"
+        }
     };
 
     [Theory]
     [InlineData("hello.debug.dll", "hello")]
     [InlineData("hello.release.dll", "hello")]
-    [InlineData("hello.sub.dll", "hello")]
+    [InlineData("hello.sub.dll", "hello.sub")]
     public void ReadStaticVoidMain(string dll, string key)
     {
         using var dll_stream = Utilities.GetResource(dll);
