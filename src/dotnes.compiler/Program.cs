78eed8373980b75acc15ef1b461725fdca209f20
Charles Betros <charles.betros@gmail.com>
2024-08-27 16:12:42 -0500

    `dotnes.exe` console compiler (#40)
    Fixes: https://github.com/jonathanpeppers/dotnes/issues/41
    
    The project that you want to transpile will have to be built first to run this against the output assembly. Running the transpiler using this cli makes debugging really simple though:
    
        Usage: dotnes.exe mydll.dll chr_generic.s
    
    Co-authored-by: Charles Betros <charles.betros@hcss.com>

---
+++ src/dotnes.compiler/Program.cs
@@ -0,0 +1,47 @@
+﻿using dotnes;
+
+var logger = new ConsoleLogger();
+
+string targetPath = "";
+string assemblyFile = "";
+string outputPath = "";
+
+if (args.Length < 2
+    || string.IsNullOrWhiteSpace(args[0])
+    || string.IsNullOrWhiteSpace(args[1]))
+{
+    Console.WriteLine("Usage: dotnes.exe mydll.dll chr_generic.s");
+    return;
+}
+
+targetPath = args[0];
+assemblyFile = args[1];
+
+if (!File.Exists(targetPath))
+{
+    logger.WriteError($"The target path '{targetPath}' does not exist.");
+    return;
+}
+
+if (!File.Exists(assemblyFile))
+{
+    logger.WriteError($"The assembly file '{assemblyFile}' does not exist.");
+    return;
+}
+
+outputPath = Path.ChangeExtension(targetPath, "nes");
+
+logger.WriteStatus("Running compiler.");
+logger.WriteStatus($"Target Path: {targetPath}");
+logger.WriteStatus($"Assembly File: {assemblyFile}");
+logger.WriteStatus($"Output Path: {outputPath}");
+
+var transpiler = new TranspileToNES
+{
+    DiagnosticLogging = true,
+    AssemblyFiles = [assemblyFile],
+    TargetPath = targetPath,
+    OutputPath = outputPath,
+    Logger = logger
+};
+transpiler.Execute();
