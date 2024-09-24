978d612d667090c34b316875a7f1600d85dbeb8a
Jonathan Peppers <jonathan.peppers@microsoft.com>
2024-09-24 16:25:25 -0500

    Use System.Reflection for GetNumberOfArguments, HasReturnValue
    This prevents us from hardcoding things in a `switch` statement, but will be slightly slower for each initial call.

---
+++ src/dotnes.tasks/Utilities/ReflectionCache.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace dotnes;
+
+class ReflectionCache
+{
+    readonly Dictionary<string, MethodInfo> _cache = [];
+
+    public MethodInfo GetMethod(string name)
+    {
+        if (!_cache.TryGetValue(name, out var method))
+        {
+            if (name != nameof(NESLib.vram_write))
+            {
+                _cache[name] = method = typeof(NESLib).GetMethod(name) ??
+                    throw new InvalidOperationException($"Unable to find method named '{nameof(NESLib)}.{name}'!");
+            }
+            else
+            {
+                //TODO: this isn't great, vram_write() has overloads for string and byte[], just using string here
+                _cache[name] = method = typeof(NESLib).GetMethod(name, [typeof(string)]) ??
+                    throw new InvalidOperationException($"Unable to find method named '{nameof(NESLib)}.{name}'!");
+            }
+        }
+        return method;
+    }
+
+    public int GetNumberOfArguments(string name) => GetMethod(name).GetParameters().Length;
+
+    public bool HasReturnValue(string name) => GetMethod(name).ReturnType != null;
+}
